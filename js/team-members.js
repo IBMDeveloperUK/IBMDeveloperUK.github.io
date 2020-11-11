39ef6939c4030bf1fff641d6390ec47d8747e881
Liam Hampton <liam.hampton@ibm.com>
2020-11-11 15:50:13 +0000

    create team members with javascript

---
+++ js/team-members.js
@@ -0,0 +1,111 @@
+data = [
+  {
+    name: "Liam",
+    tech: "Golang | Open Source",
+    twitter: "https://twitter.com/LiamConroyH",
+    github: "https://github.com/liamchampton",
+    linkedin: "https://www.linkedin.com/in/liam-conroy-hampton/",
+    bio:
+      "Liam focuses on developer advocacy with a keen interest in Golang and open source",
+  },
+
+  {
+    name: "Yamini",
+    tech: "Data Science | Node-RED | Watson API's",
+    twitter: "https://twitter.com/yaminigrao",
+    github: "https://github.com/YaminiRao",
+    linkedin: "https://www.linkedin.com/in/yamini-rao/",
+    bio:
+      "As a Developer Advocate,Yamini compiles developer scenarios, workshops and training material",
+  },
+
+  {
+    name: "Ross",
+    tech: "IOT | Node-RED | R",
+    twitter: "https://twitter.com/rcruicks",
+    github: "https://github.com/ibmrcruicks",
+    linkedin: "https://www.linkedin.com/in/ross-cruickshank/",
+    bio:
+      "Ross is a Developer Advocate, helping developers (startups through enterprise)",
+  },
+
+  {
+    name: "Ed",
+    tech: "Cloud and Containers",
+    twitter: "https://twitter.com/ukcloudman",
+    github: "https://github.com/edshee",
+    linkedin: "https://www.linkedin.com/in/edshee/",
+    bio:
+      "As A Developer Advocate, Ed has a focus on cloud native computing and works extensively with Docker",
+  },
+
+  {
+    name: "Sean",
+    tech: "Artificial Intelligence | IOT | Node-RED | Python",
+    twitter: "https://twitter.com/seanmtraceyH",
+    github: "https://github.com/seanmtracey/",
+    linkedin: "https://www.linkedin.com/in/seanmtracey/",
+    bio:
+      "Sean's career has been focused around rapid prototyping and MVP creation",
+  },
+
+  {
+    name: "Margriet",
+    tech: "Data Science | Deep Learning |Python",
+    twitter: "https://twitter.com/MargrietGr",
+    github: "https://github.com/MargrietGroenendijk",
+    linkedin: "https://www.linkedin.com/in/margrietgroenendijk/",
+    bio:
+      "Margriet is a Data & AI Developer Advocate. She develops and presents talks",
+  },
+
+  {
+    name: "Matt",
+    tech: "Blockchain | Machine Learning | Data Science",
+    twitter: "https://twitter.com/HammerToe",
+    github: "https://github.com/hammertoe",
+    linkedin: "https://www.linkedin.com/in/matthamilton77/",
+    bio:
+      "Matt is a developer advocate based in Bristol with an interest in machine learning",
+  },
+
+  {
+    name: "Simon",
+    tech: "Integrated Accounts",
+    twitter: "https://twitter.com/SimonARBaker",
+    github: "https://github.com/SimonARBaker",
+    linkedin: "https://www.linkedin.com/in/simonarbaker/",
+    bio:
+      "Simon is a Client Developer Advocate guiding Integrated Account teams in UKI",
+  },
+
+  {
+    name: "Andrea",
+    tech: "Containers | Serverless",
+    twitter: "https://twitter.com/blackchip76",
+    github: "https://github.com/afrittoli",
+    linkedin: "https://www.linkedin.com/in/andreafrittoli/",
+    bio:
+      "Andrea is a Developer Advocate at IBM, a hacker and open source enthusiast",
+  },
+
+  {
+    name: "Angela",
+    tech: "Programme Manager",
+    twitter: "https://twitter.com/angelajbates",
+    github: "https://github.com/IBMDeveloperUK",
+    linkedin: "https://www.linkedin.com/in/angelajbates/",
+    bio:
+      "Developer programmes, Influencer relations and marketing,Business Development",
+  },
+
+  {
+    name: "Mo",
+    tech: "Head of Developer Advocacy (Europe)",
+    twitter: "https://twitter.com/mohaghighi",
+    github: "https://github.com/mohaghighi",
+    linkedin: "https://www.linkedin.com/in/mohaghighi/",
+    bio:
+      "Dr Mo Haghighi leads IBM's Developer Ecosystems Group in Europe. His responsibilities include leading several teams of developer advocates, community managers and business development managers in UKI, Benelux, DACH, France, SPGi, Italy, CEE, and overseeing all advocacy operations and developer-related activities in Europe.",
+  },
+];
