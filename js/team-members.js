ccd037981ede7e39d1afc79f6ba7cfaacef42fe5
Liam Hampton <liam.hampton@ibm.com>
2020-11-11 16:20:55 +0000

    extract js into a file

--- js/team-members.js
+++ js/team-members.js
@@ -1,6 +1,7 @@
-data = [
+var data = [
   {
     name: "Liam",
+    pictureTitle: "Picture of Liam and his tech stack",
     tech: "Golang | Open Source",
     twitter: "https://twitter.com/LiamConroyH",
     github: "https://github.com/liamchampton",
@@ -11,16 +12,18 @@ data = [
 
   {
     name: "Yamini",
+    pictureTitle: "Picture of Yamini and her tech stack",
     tech: "Data Science | Node-RED | Watson API's",
     twitter: "https://twitter.com/yaminigrao",
     github: "https://github.com/YaminiRao",
     linkedin: "https://www.linkedin.com/in/yamini-rao/",
     bio:
-      "As a Developer Advocate,Yamini compiles developer scenarios, workshops and training material",
+      "As a Developer Advocate, Yamini compiles developer scenarios, workshops and training material",
   },
 
   {
     name: "Ross",
+    pictureTitle: "Picture of Ross and his tech stack",
     tech: "IOT | Node-RED | R",
     twitter: "https://twitter.com/rcruicks",
     github: "https://github.com/ibmrcruicks",
@@ -31,6 +34,7 @@ data = [
 
   {
     name: "Ed",
+    pictureTitle: "Picture of Ed and his tech stack",
     tech: "Cloud and Containers",
     twitter: "https://twitter.com/ukcloudman",
     github: "https://github.com/edshee",
@@ -41,6 +45,7 @@ data = [
 
   {
     name: "Sean",
+    pictureTitle: "Picture of Sean and his tech stack",
     tech: "Artificial Intelligence | IOT | Node-RED | Python",
     twitter: "https://twitter.com/seanmtraceyH",
     github: "https://github.com/seanmtracey/",
@@ -51,6 +56,7 @@ data = [
 
   {
     name: "Margriet",
+    pictureTitle: "Picture of Margriet and her team role",
     tech: "Data Science | Deep Learning |Python",
     twitter: "https://twitter.com/MargrietGr",
     github: "https://github.com/MargrietGroenendijk",
@@ -61,6 +67,7 @@ data = [
 
   {
     name: "Matt",
+    pictureTitle: "Picture of Matt and his tech stack",
     tech: "Blockchain | Machine Learning | Data Science",
     twitter: "https://twitter.com/HammerToe",
     github: "https://github.com/hammertoe",
@@ -71,6 +78,7 @@ data = [
 
   {
     name: "Simon",
+    pictureTitle: "Picture of Simon and his team role",
     tech: "Integrated Accounts",
     twitter: "https://twitter.com/SimonARBaker",
     github: "https://github.com/SimonARBaker",
@@ -81,6 +89,7 @@ data = [
 
   {
     name: "Andrea",
+    pictureTitle: "Picture of Andre and his tech stack",
     tech: "Containers | Serverless",
     twitter: "https://twitter.com/blackchip76",
     github: "https://github.com/afrittoli",
@@ -91,6 +100,7 @@ data = [
 
   {
     name: "Angela",
+    pictureTitle: "Picture of Angela and her team role",
     tech: "Programme Manager",
     twitter: "https://twitter.com/angelajbates",
     github: "https://github.com/IBMDeveloperUK",
@@ -101,6 +111,7 @@ data = [
 
   {
     name: "Mo",
+    pictureTitle: "Picture of Mo and his team role",
     tech: "Head of Developer Advocacy (Europe)",
     twitter: "https://twitter.com/mohaghighi",
     github: "https://github.com/mohaghighi",
@@ -109,3 +120,85 @@ data = [
       "Dr Mo Haghighi leads IBM's Developer Ecosystems Group in Europe. His responsibilities include leading several teams of developer advocates, community managers and business development managers in UKI, Benelux, DACH, France, SPGi, Italy, CEE, and overseeing all advocacy operations and developer-related activities in Europe.",
   },
 ];
+
+console.log("team profile data array: ", data);
+
+let listData = data,
+  // Set up the loop that goes through the array 1 index at a time
+  numberOfListItems = listData.length,
+  i;
+
+for (i = 0; i < numberOfListItems; i++) {
+  console.log("in loop");
+  mediaContainer = document.createElement("div");
+  mediaContainer.className = "media";
+  document.getElementById("team-member-container").appendChild(mediaContainer);
+
+  teamMemberCol = document.createElement("div");
+  teamMemberCol.className = "team-member-col";
+  mediaContainer.appendChild(teamMemberCol);
+
+  teamMember = document.createElement("div");
+  teamMember.className = "team-member-" + listData[i].name + " team-member ";
+  teamMember.title = listData[i].pictureTitle;
+  teamMemberCol.appendChild(teamMember);
+
+  techStack = document.createElement("span");
+  techStack.innerHTML = listData[i].tech;
+  teamMember.appendChild(techStack);
+
+  memberName = document.createElement("h2");
+  memberName.innerHTML = listData[i].name;
+  teamMemberCol.appendChild(memberName);
+
+  socialLinks = document.createElement("h3");
+  teamMemberCol.appendChild(socialLinks);
+
+  twitterLink = document.createElement("a");
+  twitterLink.className = "social-button";
+  twitterLink.href = listData[i].twitter;
+  twitterLink.target = "_blank";
+  socialLinks.appendChild(twitterLink);
+
+  twitterPic = document.createElement("img");
+  twitterPic.src = "assets/img/twitter.png";
+  twitterLink.appendChild(twitterPic);
+
+  githubLink = document.createElement("a");
+  githubLink.className = "social-button";
+  githubLink.href = listData[i].github;
+  githubLink.target = "_blank";
+  socialLinks.appendChild(githubLink);
+
+  githubPic = document.createElement("img");
+  githubPic.src = "assets/img/github.png";
+  githubLink.appendChild(githubPic);
+
+  linkedinLink = document.createElement("a");
+  linkedinLink.className = "social-button";
+  linkedinLink.href = listData[i].linkedin;
+  linkedinLink.target = "_blank";
+  socialLinks.appendChild(linkedinLink);
+
+  linkedinPic = document.createElement("img");
+  linkedinPic.src = "assets/img/linkedin.png";
+  linkedinLink.appendChild(linkedinPic);
+
+  lineBreak = document.createElement("br");
+  teamMemberCol.appendChild(lineBreak);
+
+  teamMemberBio = document.createElement("div");
+  teamMemberBio.className = "post";
+  teamMemberBio.innerHTML = listData[i].bio;
+  teamMemberCol.appendChild(teamMemberBio);
+
+  threeDots = document.createElement("span");
+  threeDots.className = "dots";
+  threeDots.innerHTML = "...";
+  teamMemberBio.appendChild(threeDots);
+
+  readMore = document.createElement("button");
+  readMore.className = "read";
+  readMore.innerHTML = "read more";
+  teamMemberBio.appendChild(readMore);
+}
